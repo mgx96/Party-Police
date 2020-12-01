4a0e44b0b03affb694c39257fe7e05379785bce4
Stefan <sovergaard96@gmail.com>
2020-11-30 21:47:00 -0800

    Added a Half-Cooked Dialogue System
    Dialogue Barebones, Some Movement changes to do with Dialogue, and that's it.

---
+++ Party Police/Assets/Scripts/TestScripts/NPC.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+
+    private bool canTalk = false;
+    private bool isTalking = false;
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+        }
+
+        canTalk = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+        }
+
+        canTalk = false;
+        isTalking = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && canTalk && isTalking == false)
+        {
+            gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+            isTalking = true;
+        }
+    }
+
+}
--- Party Police/Assets/Scripts/TestScripts/StefanPlayer.cs
+++ Party Police/Assets/Scripts/TestScripts/StefanPlayer.cs
@@ -3,14 +3,14 @@
 public class StefanPlayer : MonoBehaviour
 {
 
-    public GameObject background;
+    public GameObject border;
 
     void Update()
     {
-        float xBorder = (background.transform.localScale.x)/2;
-        float yBorder = (background.transform.localScale.y)/2;
+        float xBorder = (border.transform.localScale.x)/2;
+        float yBorder = (border.transform.localScale.y)/2;
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, -xBorder, xBorder), Mathf.Clamp(transform.position.y, -yBorder, yBorder));
-
-        //GUIUtility.ExitGUI();
     }
+
+    
 }
